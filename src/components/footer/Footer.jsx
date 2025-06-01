78ba43c15e2ca8d915ea4b3a38ccd0d834569a58
cosmas <cosmaschilewani@gmail.com>
2025-06-01 20:41:12 +0200

    changed background image on home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Footer.css'; // Import the CSS file for custom styling
+import './Footer.css'; 
 
 const Footer = () => {
   return (
@@ -8,14 +8,12 @@ const Footer = () => {
         <div className="row">
           {/* Column 1: About Us */}
           <div className="col-md-4">
-            <h5>About Us</h5>
+            <h5>About App</h5>
             <p>
-              We are a company dedicated to providing the best services and products to our customers. 
-              Our mission is to make your life easier and better.
+            Making global exploration easy—right at your fingertips.
             </p>
           </div>
 
-          {/* Column 2: Quick Links */}
           <div className="col-md-4">
             <h5>Quick Links</h5>
             <ul className="list-unstyled">
@@ -25,18 +23,16 @@ const Footer = () => {
             </ul>
           </div>
 
-          {/* Column 3: Contact Info */}
           <div className="col-md-4">
             <h5>Contact Us</h5>
             <ul className="list-unstyled">
-              <li><i className="fas fa-map-marker-alt"></i> 123 Main Street, City, Country</li>
-              <li><i className="fas fa-phone"></i> +1 (123) 456-7890</li>
-              <li><i className="fas fa-envelope"></i> info@example.com</li>
+              <li><i className="fas fa-map-marker-alt"></i> Admarc Street Chilomoni,Blantyre,Malawi</li>
+              <li><i className="fas fa-phone"></i>  (+265) 991-644-803</li>
+              <li><i className="fas fa-envelope"></i>cosmaschilewani@gmail.com</li>
             </ul>
           </div>
         </div>
 
-        {/* Footer Bottom */}
         <div className="row mt-3">
           <div className="col-12 text-center">
             <p className="mb-0">&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
