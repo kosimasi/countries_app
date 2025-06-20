c779cedceeb88bf7a66faa71f70db374017da605
cosmas <cosmaschilewani@gmail.com>
2025-06-20 21:18:24 +0200

    fix errors caused by update in the endpoint

--- src/components/utils/api.js
+++ src/components/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // The API endpoint am using now requires all fields query parameter.
 // const API_URL = "https://restcountries.com/v3.1/all";
 const API_URL =
-  "https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,area,languages,currencies";
+  "https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,area,languages,currencies,capital,cca3";
 
 const fetchCountries = async () => {
   const response = await axios.get(API_URL);
