b8ae65360f440febc450d768fe3ed0ebed21fe69
cosmas <cosmaschilewani@gmail.com>
2025-06-20 20:30:32 +0200

    The API endpoint am using now requires all fields query parameter

--- src/components/utils/api.js
+++ src/components/utils/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-
-const API_URL = "https://restcountries.com/v3.1/all";
+// The API endpoint am using now requires all fields query parameter.
+// const API_URL = "https://restcountries.com/v3.1/all";
+const API_URL =
+  "https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,area,languages,currencies";
 
 const fetchCountries = async () => {
   const response = await axios.get(API_URL);
